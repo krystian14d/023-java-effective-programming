e12ba50c3e9db9eb9b1cdcd331a3b51f851813a9
Krystian <krystian14d@gmail.com>
2023-01-30 10:13:52 +0100

    23. Use hierarchy instead of Class fields

---
+++ src/main/java/useHierarchyInsteadOfClassFields/Figure.java
@@ -0,0 +1,40 @@
+package useHierarchyInsteadOfClassFields;
+
+abstract class Figure {
+    abstract double area();
+}
+
+class Circle extends Figure {
+    final double radius;
+
+    Circle (double radius){
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * (radius * radius);
+    }
+}
+
+class Rectangle extends Figure{
+    final double length;
+    final double width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double area() {
+        return length * width;
+    }
+}
+
+class Square extends Rectangle{
+    Square(double side){
+        super(side, side);
+    }
+}
+
